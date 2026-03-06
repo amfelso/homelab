8ce6d80c3fdac856e3baeb897e44e8f2407b15f6
August Felso <77752049+amfelso@users.noreply.github.com>
2026-03-05 21:14:42 -0500

    Draft out main logic

--- app/main.go
+++ app/main.go
@@ -1,11 +1,32 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"log"
+	"time"
+
+	collectors "github.com/amfelso/homelab/pi-agent/collectors"
+)
 
 func main() {
-	run()
-}
+	var seconds int
 
-func run() {
-	log.Println("Hello, World!")
+	flag.IntVar(&seconds, "seconds", 300, "the time between metric collection cycles")
+
+	flag.Parse()
+	log.Printf("Seconds: %d\n", seconds)
+
+	metrics := []collectors.Collector{&collectors.Cpu{}}
+
+	ticker := time.NewTicker(time.Duration(seconds) * time.Second)
+	for range ticker.C {
+		for _, c := range metrics {
+			val, err := c.Collect()
+			if err != nil {
+				log.Printf("Couldn't get metric value: %s\n", err)
+				continue
+			}
+			log.Printf("Found metric value: %.2f\n", val)
+		}
+	}
 }
