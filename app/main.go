60a6e167330f35210319e7484ca6e4698423c843
August Felso <77752049+amfelso@users.noreply.github.com>
2026-03-06 06:18:28 -0500

    Add temperature metric

--- app/main.go
+++ app/main.go
@@ -9,16 +9,17 @@ import (
 )
 
 func main() {
-	var seconds int
+	var schedule int
 
-	flag.IntVar(&seconds, "seconds", 300, "the time between metric collection cycles")
+	flag.IntVar(&schedule, "schedule", 300, "seconds between metric collection cycles")
 
 	flag.Parse()
-	log.Printf("Seconds: %d\n", seconds)
+	log.Printf("Schedule: %d seconds\n", schedule)
 
-	metrics := []collectors.Collector{collectors.NewCpu("/host/proc/stat"), collectors.NewMemory("/host/proc/meminfo")}
+	metrics := []collectors.Collector{collectors.NewCpu("/host/proc/stat"), collectors.NewMemory("/host/proc/meminfo"),
+									  collectors.NewTemperature("/host/sys/class/thermal/thermal_zone0/temp")}
 
-	ticker := time.NewTicker(time.Duration(seconds) * time.Second)
+	ticker := time.NewTicker(time.Duration(schedule) * time.Second)
 	for range ticker.C {
 		for _, c := range metrics {
 			val, err := c.Collect()
@@ -26,7 +27,7 @@ func main() {
 				log.Printf("Couldn't get metric value: %s\n", err)
 				continue
 			}
-			log.Printf("Found metric value: %.2f\n", val)
+			log.Printf("%s: %.2f\n", c.Name(), val)
 		}
 	}
 }
